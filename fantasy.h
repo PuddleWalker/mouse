e95ab0b3d9479c387801fdba30c83fd5fbe046ee
Тимур Миндубаев <mindubaev.timurka@gmail.com>
2024-03-20 23:19:51 +0300

    Добавьте файлы проекта.

---
+++ fantasy.h
@@ -0,0 +1,44 @@
+﻿#pragma once
+
+#include <iostream>
+#include <string>
+#include <initializer_list>
+
+using namespace std;
+
+template <typename T>
+class calendar
+{
+
+protected:
+	T month = 0;
+	int year = 0;
+	int* dates = new int[30];
+public:
+	calendar(int year, T month) : month{ month }, year{ year } {}
+	calendar(initializer_list<int> dates)
+	{
+		int i = 0;
+		for (const auto& e : dates)
+		{
+			cout << e << " ";
+			this->dates[i] = e;
+			i++;
+		}
+	}
+
+	void print() const
+	{
+		cout << "Важные даты: " << endl;
+		cout << "Год: " << year << endl;
+		cout << "Месяц: " << month << endl;
+	}
+
+	void printb() {
+		for (const auto& value : dates) {
+			cout << value << ' ';
+		}
+		std::cout << std::endl;
+	}
+
+};
---
+++ mouse.cpp
@@ -0,0 +1,40 @@
+﻿
+#include "fantasy.h"
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    int year;
+    string month;
+    int* dates = new int[10];
+
+    cout << "Программа для записи важных дат!" << endl;
+
+    cout << endl;
+
+    cout << "Введите год: "; cin >> year;
+
+    cout << "Введите месяц: ";
+    getline(cin, month);
+
+    cout << endl;
+
+    cout << "Заполните важные даты";
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "Введите важную дату (или 0 для завершения)\n"; cin >> dates[i];
+        while (dates[i] > 31) {
+            cout << "Неверная дата. Попробуйте снова"; cin >> dates[i];
+            if (dates[i] == 0) break;
+        }
+    }
+
+    calendar<string> a{ year, month };
+    a.print();
+    calendar<int> b{ *dates };
+
+
+
+
+
+}
